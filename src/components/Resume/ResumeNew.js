e56514f13f56a8b9c15e81286eadb0b076fc8762
Shaik Rehan <abacusrehan@gmail.com>
2022-05-19 23:53:38 +0530

    Updated PDF URl

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/ShaikRehan123/resume/main/Internshala_Resume.pdf";
+  "https://raw.githubusercontent.com/ShaikRehan123/ammi_portofolio/main/src/Assets/myresume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
